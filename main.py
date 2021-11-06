166650521220eb340dc69c6118160c999569f678
Uday <ujadeja86@gmail.com>
2021-11-06 15:26:46 +0530

    new main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from wsgiref import simple_server
 from flask import Flask, request, render_template
 from flask import Response
 import os
-from flask_cors import CORS, cross_origin
 from prediction_Validation_Insertion import pred_validation
 from trainingModel import trainModel
 from training_Validation_Insertion import train_validation
@@ -17,15 +16,15 @@ app = Flask(__name__)
 
 dashboard.bind(app)
 
-CORS(app)
+
 
 @app.route("/", methods=['GET'])
-@cross_origin()
+
 def home():
     return render_template('index.html')
 
 @app.route("/predict", methods=['POST'])
-@cross_origin()
+
 def predictRouteClient():
     try:
         if request.form is not None:
@@ -58,7 +57,7 @@ def predictRouteClient():
 
 
 @app.route("/train", methods=['POST'])
-@cross_origin()
+
 def trainRouteClient():
 
     try:
