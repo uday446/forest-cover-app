da54f5c0502a3baf7d417c3a8d5ab9eb40fb6cd2
Uday <ujadeja86@gmail.com>
2021-11-08 13:37:29 +0530

    few changes

--- driveUpload.py
+++ driveUpload.py
@@ -78,7 +78,7 @@ class upload_training:
             path = "Prediction_Batch_Files/"+name
 
             df.to_csv(name, index=None, header=True, mode='w')
-            self.change_permissions_recursive("Prediction_Batch_Files","0o777")
+            self.change_permissions_recursive("Prediction_Batch_Files",0o777)
             shutil.copy(name, "Prediction_Batch_Files")
 
             self.log_writer.log(self.file_object, 'exited uploadfile_predict of driveUpload.py!!')
@@ -120,8 +120,8 @@ class upload_training:
         except Exception as e:
             return e
 
-    def change_permissions_recursive(path, mode):
-        for root, dirs, files in os.walk(path, topdown=False):
+    def change_permissions_recursive(pat, mode):
+        for root, dirs, files in os.walk(pat, topdown=False):
             for dir in [os.path.join(root, d) for d in dirs]:
                 os.chmod(dir, mode)
 
